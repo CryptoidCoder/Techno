9b56d7dee1a5cfb474a41472184677dc1fdbb003
Josh Leach <45842827+CryptoidCoder@users.noreply.github.com>
2021-10-16 13:22:21 +0100

    Initial Commit

---
+++ main.basis.py
@@ -0,0 +1,14 @@
+##Functions:
+
+## Audio:
+#get audio input
+#save audio input to file
+#process audio input (via process.py)
+#output message (via a log file)
+
+## Physical:
+#switch relay(s)
+#
+
+## Software:
+#
---
+++ process.py
@@ -0,0 +1,6 @@
+##Functions:
+
+#save query locally
+#run if functions on query
+#if something for the pi, do it
+#if something for another pc, @it on discord, and share data via webhook
