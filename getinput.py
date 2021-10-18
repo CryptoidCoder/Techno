68f5d2db1d9cf02e79af59776e8d9458e0a27a1d
Josh Leach <45842827+CryptoidCoder@users.noreply.github.com>
2021-10-18 20:13:32 +0100

    Updates 1.0 - Added Audio Input Functions + Logging Functionality

---
+++ getinput.py
@@ -0,0 +1,150 @@
+import pyttsx3 # for text-to-speech
+import webbrowser #for opening web pages
+import smtplib #for emailing
+import random # to pick random
+import speech_recognition as s_r #for speech-to-text
+import wikipedia #to look stuff up
+import datetime #to find the date+time
+import os # to open programs
+import sys # to use the system
+import time #so you can wait
+
+from pynput.keyboard import Key, Controller, Listener #to use the media keys
+from dotenv import load_dotenv
+import requests #to send web requests
+
+
+load_dotenv()
+
+
+#setup pynput
+pynputkeyboard = Controller()
+
+# Initialize speech 
+engine = pyttsx3.init('sapi5')
+
+
+#set the username
+Master = os.getenv('Master')
+
+#what voice the text to speech will use
+american = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_ZIRA_11.0" #Path to the american voice
+british = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-GB_HAZEL_11.0" #Path to the british voice
+speakingvoice = british # default speaking voice is british
+
+
+wakeword = os.getenv('WakeWord')
+
+#set the speaking engine
+voices = engine.getProperty('voices')
+#set the words per minute rate of speech engine
+engine.setProperty('voice', speakingvoice)
+
+def addnewline(filename,text): #append text to a new line on a file
+    # Open the file in append & read mode ('a+')
+    with open(filename, "a+") as file_object:
+        # Move read cursor to the start of file.
+        file_object.seek(0)
+        # If file is not empty then append '\n'
+        data = file_object.read(100)
+        if len(data) > 0 :
+            file_object.write("\n")
+        # Append text at the end of file
+        file_object.write(text)
+        file_object.close()
+
+
+def justspeak(audio): # speak what is inputed
+    engine.say(audio)
+    engine.runAndWait()
+
+def printspeak(text): # speak and print what is inputed
+    print(text)
+    engine.say(text)
+    engine.runAndWait()
+
+def greetMe(): #say either good morning, good afternoon or good evening depending on the time.
+    currentH = int(datetime.datetime.now().hour)
+    if currentH >= 0 and currentH < 12:
+        printspeak(f'Good Morning {Master}!')
+
+    if currentH >= 12 and currentH < 18:
+        printspeak(f'Good Afternoon {Master}!')
+
+    if currentH >= 18 and currentH !=0:
+        printspeak(f'Good Evening {Master}!')
+
+def myCommand(): #turn sound into text
+    r = s_r.Recognizer()
+    my_mic = s_r.Microphone(device_index=1) #my device index is 1, you have to put your device index
+    try:
+        with my_mic as source:
+            printspeak("Listening...")
+            r.adjust_for_ambient_noise(source) #reduce noise
+            audio = r.listen(source) #take voice input from the microphone
+            query = (r.recognize_google(audio))
+            query = query.lower()
+            log(query)
+
+    except:
+        printspeak("Error taking in audio.") 
+    
+    return query
+
+def log(query): #output datetime, query to query.log file
+    datetimenow = datetime.datetime.now()
+    datetimenow = str(datetimenow)
+    message = "At Time: " + datetimenow + " Techno Recieved: " + query
+    addnewline("query.log",message)
+    return message
+
+def fetchlog(): #get latest query from query.log file
+    with open('query.log', 'r') as f:
+        last_line = f.readlines()[-1]
+        query = last_line[53:]
+        return query
+
+def startup(): #what to do on startup
+    greetMe()
+    printspeak("Hello My Name is Techno, Your Virtual Helper.")
+    #query = myCommand()
+
+
+
+startup()
+
+
+while True:
+    while True: #when wakeword spoke, take query input & save it, then thank.
+
+        def show(key):
+            if key == Key.delete:
+                sys.exit()
+                return False
+
+        with Listener(on_press = show) as listener:
+            listener.join()
+
+        r = s_r.Recognizer()
+        my_mic = s_r.Microphone(device_index=1) #my device index is 1, you have to put your device index
+        try:
+            with my_mic as source:
+                r.adjust_for_ambient_noise(source) #reduce noise
+                audio = r.listen(source) #take voice input from the microphone
+            query = (r.recognize_google(audio))
+            query = query.lower()
+            if wakeword in query:
+                printspeak("WakeWord Activated")
+                break
+
+            if 'exit' in query or 'stop' in query or 'end' in query:
+                printspeak("Exiting Session...")
+                sys.exit()
+
+        except:
+            print("No Wakeword...")
+
+    myCommand()
+
+
+    printspeak("Thanks.")
--- process.py
+++ process.py
@@ -4,3 +4,35 @@
 #run if functions on query
 #if something for the pi, do it
 #if something for another pc, @it on discord, and share data via webhook
+
+
+import datetime #to find the date+time
+import sys # to use the system
+
+def fetchlog(): #get latest query from query.log file
+    try:
+        with open('query.log', 'r') as f:
+            last_line = f.readlines()[-1]
+            query = last_line[53:]
+            return query
+
+    except:
+        print("Error Reading query.log")
+        return "Error"
+
+
+query = fetchlog()
+
+if 'test' in query:
+    print(f"This is a test message: {query}")
+
+elif 'what is the time' in query or 'tell me the time' in query:
+    print(datetime.datetime.now)
+
+
+elif 'exit' in query or 'stop' in query or 'end' in query:
+    print("Exiting Session...")
+    sys.exit()
+
+elif 'Error' in query:
+    print("Error Has Been Returned - Not Able To Read From query.log File.")
